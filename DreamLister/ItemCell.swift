44b60cac20a837bb6d5d1e1763cce7452eefc3e3
DutchRascal <Andre@imac.fritz.box>
2017-01-23 19:31:26 +0100

    05 - 73

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Andre Boevink on 23/01/2017.
+//  Copyright © 2017 Andre Boevink. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    
+}
