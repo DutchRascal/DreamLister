791f55775ab16bd1e7fba0acc5250afa85d2dc7e
DutchRascal <Andre@imac.fritz.box>
2017-01-23 17:43:26 +0100

    05 - 71

---
+++ DreamLister/Item+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Item+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Andre Boevink on 23/01/2017.
+//  Copyright © 2017 Andre Boevink. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Item: NSManagedObject {
+
+}
---
+++ DreamLister/ItemType+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ItemType+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Andre Boevink on 23/01/2017.
+//  Copyright © 2017 Andre Boevink. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class ItemType: NSManagedObject {
+
+}
