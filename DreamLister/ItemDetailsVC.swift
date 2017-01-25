c6edb87e8a70bac151ab04b735832ac8a5c4ef8d
DutchRascal <Andre@imac-2.fritz.box>
2017-01-25 19:40:31 +0100

    05 - 80

--- DreamLister/ItemDetailsVC.swift
+++ DreamLister/ItemDetailsVC.swift
@@ -9,14 +9,17 @@
 import UIKit
 import CoreData
 
-class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var storePicker: UIPickerView!
     @IBOutlet weak var titleField: CustomTextField!
     @IBOutlet weak var priceField: CustomTextField!
     @IBOutlet weak var detailsField: CustomTextField!
+    @IBOutlet weak var thumgImg: UIImageView!
     
     var stores = [Store]()
+    var itemToEdit: Item?
+    var imagePicker: UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,28 +30,34 @@ class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDeleg
         
         storePicker.delegate = self
         storePicker.dataSource = self
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
         
-        //        let store = Store(context: context)
-        //        store.name = "Best Buy"
+        //                let store = Store(context: context)
+        //                store.name = "Best Buy"
         //
-        //        let store2 = Store(context: context)
-        //        store2.name = "Tesla Dealership"
+        //                let store2 = Store(context: context)
+        //                store2.name = "Tesla Dealership"
         //
-        //        let store3 = Store(context: context)
-        //        store3.name = "Frys Electronics"
+        //                let store3 = Store(context: context)
+        //                store3.name = "Frys Electronics"
         //
-        //        let store4 = Store(context: context)
-        //        store4.name = "Target"
+        //                let store4 = Store(context: context)
+        //                store4.name = "Target"
         //
-        //        let store5 = Store(context: context)
-        //        store5.name = "Amazon"
+        //                let store5 = Store(context: context)
+        //                store5.name = "Amazon"
         //
-        //        let store6 = Store(context: context)
-        //        store6.name = "K Mart"
+        //                let store6 = Store(context: context)
+        //                store6.name = "K Mart"
         //
-        //        ad.saveContext()
+        //                ad.saveContext()
         
         getStores()
+        
+        if itemToEdit != nil {
+            loadItemData()
+        }
         
     }
     
@@ -96,8 +105,19 @@ class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDeleg
     
     @IBAction func savePressed(_ sender: UIButton) {
         
-        let item = Item(context: context)
+        var item: Item!
+        let picture = Image(context: context)
+        picture.image = thumgImg.image
         
+        if itemToEdit == nil {
+            
+            item = Item(context: context)
+        } else {
+            item = itemToEdit
+        }
+        
+        item.toImage = picture
+
         if let title = titleField.text {
             item.title = title
         }
@@ -119,9 +139,61 @@ class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDeleg
         
     }
     
+    func loadItemData() {
+        
+        if let item = itemToEdit {
+            titleField.text = item.title
+            priceField.text = "\(item.price)"
+            detailsField.text = item.details
+            
+            thumgImg.image = item.toImage?.image as? UIImage
+            
+            if let store = item.toStore {
+                
+                var index = 0
+                
+                repeat {
+                    
+                    let s = stores[index]
+                    if s.name == store.name {
+                        storePicker.selectRow(index, inComponent: 0, animated: false)
+                        break
+                    }
+                    index += 1
+                    
+                } while (index < stores.count)
+            }
+            
+        }
+        
+    }
+    
+    @IBAction func deletePressed(_ sender: UIBarButtonItem) {
+        
+        if itemToEdit != nil {
+            context.delete(itemToEdit!)
+            ad.saveContext()
+            
+        }
+        
+        _ = navigationController?.popViewController(animated: true)
+        
+    }
     
     
+    @IBAction func addImage(_ sender: UIButton) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
     
-    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let img = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            thumgImg.image = img
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
     
 }
